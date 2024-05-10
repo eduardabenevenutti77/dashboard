d0256e9e4583ffdb801786fdb02c3772e8854deb
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-05-09 21:38:51 -0300

    update

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 
 export default function FormLogin() {
+    const [email, setEmail] = useState(""); // descontrói uma váriavel
+    const [senha, setSenha] = useState(""); // useState prioriza a performance do sistema
   return (
     <Container>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Digite seu e-mail" />
+          <Form.Control type="email" placeholder="Digite seu e-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Sua senha" />
+          <Form.Control type="password" placeholder="Sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
         </Form.Group>
         <Container className="d-flex justify-content-end">
           <Button variant="primary" type="submit">
